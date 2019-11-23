95925674c3193ecb3c31fabac154686d28562e8d
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-11-23 17:31:03 +0530

    supplier component

--- src/app/setup/setup.module.ts
+++ src/app/setup/setup.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SupplierComponent } from './supplier/supplier.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SupplierComponent],
   imports: [
     CommonModule
   ]
