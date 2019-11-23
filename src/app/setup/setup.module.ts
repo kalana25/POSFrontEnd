0037a31d699f889160b99cf2a8699c2a1b0f377b
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-11-23 17:33:06 +0530

    category, product compoenents

--- src/app/setup/setup.module.ts
+++ src/app/setup/setup.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SupplierComponent } from './supplier/supplier.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductComponent } from './product/product.component';
 
 
 
 @NgModule({
-  declarations: [SupplierComponent],
+  declarations: [SupplierComponent, CategoryComponent, ProductComponent],
   imports: [
     CommonModule
   ]
