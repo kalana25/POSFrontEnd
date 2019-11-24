b4ea9ca36db7d3836b13b501e4fa378c53963c03
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-11-24 09:57:39 +0530

    supplier api class

--- src/app/models/supplier.ts
+++ src/app/models/supplier.ts
@@ -1,2 +1,12 @@
-export class Supplier {
+import { BaseEntity } from '../core/base-entity';
+export class Supplier extends BaseEntity {
+    code:string;
+    name:string;
+    description:string;
+    contactNo:string;
+    telephone:string;
+    email:string;
+    comment:string;
+    blackList:boolean;
+    active:boolean;
 }
--- src/app/services/supplier.service.ts
+++ src/app/services/supplier.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from '../core/api-service';
+import { AppSettingsService } from '../core/app-settings.service'; 
+import { Supplier } from '../models/supplier';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SupplierService {
+export class SupplierService extends ApiService<Supplier> {
 
-  constructor() { }
+  constructor(
+    protected http:HttpClient,
+    protected config:AppSettingsService
+  ) { 
+    super(config.apiUrl,"Suppliers",http);
+  }
 }
