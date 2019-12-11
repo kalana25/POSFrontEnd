8f0d6a64569a4b7b5330c064ad9c84d6c580c533
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-12-11 15:27:14 +0530

    tempary toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -16,6 +18,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SetupModule,
     BrowserAnimationsModule,
     
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
