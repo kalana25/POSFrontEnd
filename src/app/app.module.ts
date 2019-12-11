24562e5918745d77928279f5fdd62a4bfd76925a
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-12-11 16:16:53 +0530

    add some icons to tool bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     SetupModule,
     BrowserAnimationsModule,
     
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
