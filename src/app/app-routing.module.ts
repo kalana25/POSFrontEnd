3285778d5b8d950e438a62d50f53ad518738e047
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-12-11 17:00:15 +0530

    basic routing done.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SupplierComponent } from './setup/supplier/supplier.component';
+import { CategoryComponent } from './setup/category/category.component';
+import { ProductComponent } from './setup/product/product.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'supplier',component:SupplierComponent},
+  {path:'category',component:CategoryComponent},
+  {path:'product',component:ProductComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
