dea33cba28c3341463268c0343694391b481b2d0
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-12-25 21:56:34 +0530

    product form initial

--- src/app/setup/product/product-add-edit/product-add-edit.component.ts
+++ src/app/setup/product/product-add-edit/product-add-edit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup,Validator, Validators } from '@angular/forms';
+import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/product.service';
+import { Router } from '@angular/router';
+import { DropdownItem } from 'src/app/core/dropdown-item';
+import { map} from 'rxjs/operators';
+import { pipe } from 'rxjs';
 
 @Component({
   selector: 'app-product-add-edit',
@@ -7,9 +14,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductAddEditComponent implements OnInit {
 
-  constructor() { }
+  prouctFormGroup:FormGroup;
+
+  constructor(
+    private fb:FormBuilder,
+    private router:Router,
+    private productService:ProductService
+  ) { 
+
+  }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  private initForm() {
+    this.prouctFormGroup = this.fb.group({
+      'Code':['',Validators.required],
+      'Name':['',Validators.required],
+      'CategoryId':['',Validators.required],
+      'Price':[''],
+      'Barcode':[''],
+      'Active':[true]
+    });
   }
 
 }
