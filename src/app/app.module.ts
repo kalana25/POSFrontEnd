14d789ad1b4de7885b28036bec13845efab1b9f0
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2019-12-30 14:45:18 +0530

    shared module created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SetupModule } from './setup/setup.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import {MatButtonModule} from '@angular/material/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
     SetupModule,
     BrowserAnimationsModule,
     
