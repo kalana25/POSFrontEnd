7e97e54f4a3d0861bbd3d0fb8d3f5f8d7454b404
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2020-01-14 09:02:31 +0530

    category delete.

--- src/app/setup/pc-setup-board/pc-setup-board.component.ts
+++ src/app/setup/pc-setup-board/pc-setup-board.component.ts
@@ -7,6 +7,9 @@ import {MatBottomSheet, MatBottomSheetRef} from '@angular/material/bottom-sheet'
 import { BsActionMenuComponent } from './bs-action-menu/bs-action-menu.component';
 import { DropdownItem } from 'src/app/core/dropdown-item';
 import { Product } from 'src/app/models/product';
+import { DialogData } from 'src/app/core/dialog-data';
+import { DialogContentComponent } from 'src/app/shared/components/dialog-content/dialog-content.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-pc-setup-board',
@@ -24,7 +27,8 @@ export class PcSetupBoardComponent implements OnInit {
   constructor(
     protected productService:ProductService,
     protected categoryService:CategoryService,
-    protected bottomSheet:MatBottomSheet
+    protected bottomSheet:MatBottomSheet,
+    protected dialog:MatDialog
   ) { }
 
   ngOnInit() {
@@ -73,6 +77,23 @@ export class PcSetupBoardComponent implements OnInit {
     const lastCategory = this.selectionStack[length-1];
     const bottomSheetRef = this.bottomSheet.open(BsActionMenuComponent,{data:{currentLevel:this.CurrentLevel,parentCategory:lastCategory}});
 
+  }
+
+  public OnDeleteCategory(id:number) {
+    let confrimData = new DialogData()
+    confrimData.buttonCancel = true;
+    confrimData.buttonConfim = true;
+    confrimData.dialogContent = "Category Delete";
+    confrimData.dialogTitle = "Are you sure you want to delete this cateogry ?";
+    let dialogRef = this.dialog.open(DialogContentComponent,
+      {
+        width:'500px',
+        height: '180px',
+        data:confrimData
+      });
+    dialogRef.afterClosed().subscribe(res=>{
+      console.log('After close ');
+    })
   }
 
 }
