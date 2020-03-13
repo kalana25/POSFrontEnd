3f7c589f3a7d33c06cd5bff2084e8e6e4bb6ea76
Kalana Mahaarachchi <kalana.mahaarachchi@qualitapps.com>
2020-03-13 18:27:15 +0530

    with empty purchase order

--- src/app/sales/sales.module.ts
+++ src/app/sales/sales.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PurchaseOrderComponent } from './purchase-order/purchase-order.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PurchaseOrderComponent],
   imports: [
     CommonModule
   ]
