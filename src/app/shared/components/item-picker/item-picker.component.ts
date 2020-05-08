af82addac64fef3a110586d6e98822df028aca65
RKalana <kalanamahaarachchi@gmail.com>
2020-05-08 11:33:32 +0530

    add item-picker component

---
+++ src/app/shared/components/item-picker/item-picker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-picker',
+  templateUrl: './item-picker.component.html',
+  styleUrls: ['./item-picker.component.css']
+})
+export class ItemPickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DialogContentComponent } from './components/dialog-content/dialog-content.component';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatButtonModule} from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 import { noTwoWhiteSpacesValidator,numbersValidator} from 'src/app/shared/Validations/common-validation'
 import { HeaderComponent } from './components/header/header.component';
 import { AngularMaterialModule } from '../angular-material.module';
 import { RouterModule } from '@angular/router';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { ItemPickerComponent } from './components/item-picker/item-picker.component';
 
 
 @NgModule({
-  declarations: [DialogContentComponent, HeaderComponent],
+  declarations: [
+    DialogContentComponent, 
+    HeaderComponent, 
+    ItemPickerComponent
+  ],
   imports: [
     RouterModule,
     CommonModule,
