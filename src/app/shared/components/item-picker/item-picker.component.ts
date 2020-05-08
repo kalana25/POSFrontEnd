f474ae71e91033c700b76a2219b3459f809b702a
RKalana <kalanamahaarachchi@gmail.com>
2020-05-08 13:44:45 +0530

    item picker

--- src/app/shared/components/item-picker/item-picker.component.ts
+++ src/app/shared/components/item-picker/item-picker.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemPickerService } from '../../services/item-picker.service';
+import { ResponseData } from 'src/app/core/response-data';
+import { RequestData } from 'src/app/core/request-data';
+import { Item } from 'src/app/sales/models/item';
+import { PageEvent } from '@angular/material';
 
 @Component({
   selector: 'app-item-picker',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemPickerComponent implements OnInit {
 
-  constructor() { }
+  itemResponse:ResponseData<Item>;
+  itemRequest:RequestData;
+  IsLoading:boolean = false;
+
+  displayedColumns: string[] = ['code', 'name'];
+
+  constructor(
+    public itemPickerService:ItemPickerService
+  ) { }
 
   ngOnInit() {
+    this.itemRequest = new RequestData();
+    this.itemRequest.page=1;
+    this.itemRequest.pageSize=5;
+    this.getItemPagination(this.itemRequest);
+  }
+
+  private getItemPagination(supplierRequest:RequestData) {
+    this.IsLoading = true;
+    this.itemPickerService.pagination(supplierRequest)
+    .subscribe(res=>{
+      this.itemResponse = res;
+      this.IsLoading = false;
+      console.log(res);
+    },err=>{
+      this.IsLoading = false;
+      console.error(err);
+    })
+  }
+
+  public OnPage(event:PageEvent):void {
+    this.itemRequest.pageSize = event.pageSize;
+    this.itemRequest.page= event.pageIndex+1;
+    this.getItemPagination(this.itemRequest);
   }
 
 }
