34c58fc0ca18ae6fb3a08d872c4f7a1b95c0bc58
RKalana <kalanamahaarachchi@gmail.com>
2020-05-16 12:33:55 +0530

    remove created by from po

--- src/app/sales/models/purchase-order.ts
+++ src/app/sales/models/purchase-order.ts
@@ -2,6 +2,6 @@ import { BaseEntity } from '../../core/base-entity';
 export class PurchaseOrder extends BaseEntity {
     public code:string;
     public date:Date;
-    public createdBy:string;
+    public deliveryDate:Date;
     public totalPrice:number;
 }
