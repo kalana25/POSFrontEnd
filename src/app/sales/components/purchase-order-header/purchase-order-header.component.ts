4402d02112dac070256bc34dd513e62124cae846
RKalana <kalanamahaarachchi@gmail.com>
2020-05-25 15:12:05 +0530

    purchase order save fixed

--- src/app/sales/components/purchase-order-header/purchase-order-header.component.ts
+++ src/app/sales/components/purchase-order-header/purchase-order-header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Output,OnChanges, EventEmitter, Input } from '@angul
 import { FormBuilder,FormGroup,Validator, Validators } from '@angular/forms';
 import { PurchaseOrderSave } from '../../models/purchase-order-save';
 import { SharedMemoryService } from '../../../shared/services/shared-memory.service';
+import { SupplierService } from 'src/app/setup/services/supplier.service';
+import { Supplier } from 'src/app/setup/models/supplier';
 
 @Component({
   selector: 'app-purchase-order-header',
@@ -12,9 +14,11 @@ export class PurchaseOrderHeaderComponent implements OnInit,OnChanges {
   purchaseOrderHeader:FormGroup;
   @Output() purchaseOrderSaveModel = new EventEmitter<PurchaseOrderSave>();
   @Input() totalPrice;
+  public supplierList:Array<Supplier>;
 
   constructor(
     public fb:FormBuilder,
+    public supplierService:SupplierService,
     public sharedMemoryservice:SharedMemoryService
   ) { 
 
@@ -32,9 +36,11 @@ export class PurchaseOrderHeaderComponent implements OnInit,OnChanges {
       date:[new Date(),Validators.required],
       deliveryDate:[new Date()],
       code:['',Validators.required],
+      supplierId:['',Validators.required],
       user:[{value:this.sharedMemoryservice.getLoggedUserEmail(), disabled:true}],
       totalPrice:[0,Validators.required]
     });
+    this.GetSupplierData();
   }
 
   public OnSubmit() {
@@ -44,10 +50,20 @@ export class PurchaseOrderHeaderComponent implements OnInit,OnChanges {
       model.totalPrice = Number(this.purchaseOrderHeader.get('totalPrice').value);
       model.date = this.purchaseOrderHeader.get('date').value;
       model.deliveryDate = this.purchaseOrderHeader.get('deliveryDate').value;
+      model.supplierId = this.purchaseOrderHeader.get('supplierId').value;
       this.purchaseOrderSaveModel.emit(model);
     } else {
       this.purchaseOrderSaveModel.emit(null);
     }
+  }
+
+  public GetSupplierData() {
+    this.supplierService.get("findall")
+    .subscribe(res=>{
+      this.supplierList = res;
+    },err=>{
+      console.error(err);
+    })
   }
 
 }
