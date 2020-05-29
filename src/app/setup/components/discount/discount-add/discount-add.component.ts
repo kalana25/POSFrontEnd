b5e85867b763ea9f224ef6993b7ad39d6e7f8c12
RKalana <kalanamahaarachchi@gmail.com>
2020-05-29 20:29:52 +0530

    discount add complete

--- src/app/setup/components/discount/discount-add/discount-add.component.ts
+++ src/app/setup/components/discount/discount-add/discount-add.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,Router } from '@angular/router';
 import { FormBuilder,FormGroup,Validator, Validators } from '@angular/forms';
+import { DiscountInfo } from 'src/app/setup/models/discount-info';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { Product } from 'src/app/setup/models/product';
+import { DiscountSave } from 'src/app/setup/models/discount-save';
+import { DiscountService } from 'src/app/setup/services/discount.service';
 
 @Component({
   selector: 'app-discount-add',
@@ -10,10 +15,16 @@ import { FormBuilder,FormGroup,Validator, Validators } from '@angular/forms';
 export class DiscountAddComponent implements OnInit {
 
   public discountForm:FormGroup;
+  private isItemSelected:boolean;
 
 
   constructor(
-    protected fb:FormBuilder
+    protected fb:FormBuilder,
+    protected dialog:MatDialog,
+    protected route:ActivatedRoute,
+    protected router:Router,
+    protected snackBar:MatSnackBar,
+    protected discountService:DiscountService
   ) { }
 
   ngOnInit() {
@@ -22,13 +33,41 @@ export class DiscountAddComponent implements OnInit {
 
   private initForm() {
     this.discountForm = this.fb.group({
-      id:['',Validators.required ],
+      id:[''],
       itemName:['',Validators.required],
       itemId:['',Validators.required],
       rate:['',Validators.required],
       startDate:['',Validators.required],
       endDate:['',Validators.required]
     });
+  }
+
+  public OnSelectProduct(product:Product) {
+    this.discountForm.get('itemName').patchValue(product.name);
+    this.discountForm.get('itemId').patchValue(product.id);
+    this.isItemSelected = true;
+  }
+
+  public OnGoBack() {
+    this.router.navigate(['../discount-list'],{relativeTo:this.route});
+  }
+
+  public OnSave() {
+
+    if(!this.isItemSelected) {
+      this.snackBar.open("Please select the item for discount","OK",{duration:2500});
+    } else {
+      if(this.discountForm.valid){
+        let model:DiscountSave = this.discountForm.value;
+        this.discountService.add(model)
+        .subscribe(res=>{
+          this.router.navigate(['../discount-list'],{relativeTo:this.route});
+        },err=>{
+          console.error(err);
+        })
+      }
+    }
+
   }
 
 }
