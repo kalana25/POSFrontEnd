521f23fbca14de54bf939f726160eba6c93e9202
RKalana <kalanamahaarachchi@gmail.com>
2020-06-07 00:53:32 +0530

    measurement related components

---
+++ src/app/setup/components/measurement/measurement-add/measurement-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-measurement-add',
+  templateUrl: './measurement-add.component.html',
+  styleUrls: ['./measurement-add.component.css']
+})
+export class MeasurementAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/setup/components/measurement/measurement-list/measurement-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-measurement-list',
+  templateUrl: './measurement-list.component.html',
+  styleUrls: ['./measurement-list.component.css']
+})
+export class MeasurementListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
