009e08f20500a494ceac52992d25b351dc1d34a3
RKalana <kalanamahaarachchi@gmail.com>
2020-06-20 19:22:36 +0530

    components grn

---
+++ src/app/sales/components/grn/grn-list/grn-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grn-list',
+  templateUrl: './grn-list.component.html',
+  styleUrls: ['./grn-list.component.css']
+})
+export class GrnListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
