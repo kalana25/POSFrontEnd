81a60cb4b82779b93b61af86db76916fdfb66e02
RKalana <kalanamahaarachchi@gmail.com>
2020-06-21 12:44:21 +0530

    grn list completed

--- src/app/sales/components/grn/grn-list/grn-list.component.ts
+++ src/app/sales/components/grn/grn-list/grn-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ResponseData } from 'src/app/core/response-data';
+import { RequestData } from 'src/app/core/request-data';
+import { GrnPagination } from 'src/app/sales/models/grn-pagination';
+import { GoodReceivedNoteService } from 'src/app/sales/services/good-received-note.service';
+import { MatDialog, MatBottomSheet, PageEvent } from '@angular/material';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-grn-list',
@@ -7,9 +13,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GrnListComponent implements OnInit {
 
-  constructor() { }
+  grnResponse:ResponseData<GrnPagination>;
+  grnRequest:RequestData
+  IsLoading:boolean=false;
+
+  displayedColumns: string[] = ['id', 'code', 'grnDate','time','comment', 'createdByName','purchaseOrderCode','action'];
+
+  constructor(
+    protected grnService:GoodReceivedNoteService,
+    protected dialog:MatDialog,
+    protected bottomSheet:MatBottomSheet,
+    protected route:ActivatedRoute,
+    protected router:Router) { 
+
+  }
 
   ngOnInit() {
+    this.grnRequest = new RequestData();
+    this.grnRequest.page=1;
+    this.grnRequest.pageSize=5;
+    this.getGrnPagination(this.grnRequest);
+  }
+
+  public OnPage(event:PageEvent):void {
+    this.grnRequest.pageSize = event.pageSize;
+    this.grnRequest.page= event.pageIndex+1;
+    this.getGrnPagination(this.grnRequest);
+  }
+
+  private getGrnPagination(grnRequest:RequestData) {
+    this.IsLoading = true;
+    this.grnService.pagination(grnRequest)
+    .subscribe(res=>{
+      this.grnResponse = res;
+      this.IsLoading = false;
+      console.log(res);
+    },err=>{
+      this.IsLoading = false;
+      console.error(err);
+    })
+  }
+
+  public OnAddClick() {
+    this.router.navigate(['../grn-add'],{relativeTo:this.route});
   }
 
 }
