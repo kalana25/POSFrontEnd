312e78d93575eb504536a7878d18489d84b11b06
RKalana <kalanamahaarachchi@gmail.com>
2020-07-07 11:37:57 +0530

    ad hoc modifications

--- src/app/sales/components/grn/grn-additional-item/grn-additional-item.component.ts
+++ src/app/sales/components/grn/grn-additional-item/grn-additional-item.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/setup/models/product';
+import { MeasurementService } from 'src/app/setup/services/measurement.service';
+import { MatSnackBar, MatDialog } from '@angular/material';
+import { PoDetailPickerComponent } from '../../po-detail-picker/po-detail-picker.component';
+import { PurchaseOrderDetailFullItem } from 'src/app/sales/models/purchase-order-detail-fullInfo';
 
 @Component({
   selector: 'app-grn-additional-item',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GrnAdditionalItemComponent implements OnInit {
 
-  constructor() { }
+  public itemList:Array<PurchaseOrderDetailFullItem>=[];
+  constructor(
+    private measurementService:MeasurementService,
+    private snackBar:MatSnackBar,
+    private dialog:MatDialog
+  ) { }
 
   ngOnInit() {
+  }
+
+  public OnSelectProduct(product:Product) {
+
+    this.measurementService.getByItem(product.id)
+    .subscribe(mesRes=>{
+      if(mesRes.length>0) {
+
+        let dialogRef = this.dialog.open(PoDetailPickerComponent,
+        {
+          data:{'product':product,'measurement':mesRes},
+          width:'230px'
+        });
+
+        dialogRef.afterClosed().subscribe(res=>{
+          if(res) {
+            // re calculate total Price
+            // this.purchaseOrder.items.push(res);
+            this.itemList.push(res);
+            // this.tableReference.renderRows();
+            // this.purchaseOrder.totalPrice +=res.quantity*res.unitPrice;
+            // this.editForm.get('totalPrice').patchValue(this.purchaseOrder.totalPrice); 
+          }
+        });
+
+      } else {
+        this.snackBar.open("Please configure the measurements","OK",{duration:2500});
+      }
+    }, err=>{
+      console.error(err);      
+    });
+
   }
 
 }
