5ccaab55fd851bde3eaa075c3d9a8958f23ba150
RKalana <kalanamahaarachchi@gmail.com>
2020-09-07 01:28:52 +0530

    empty components

---
+++ src/app/sales/components/inventory/inventory-list-detail/inventory-list-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-list-detail',
+  templateUrl: './inventory-list-detail.component.html',
+  styleUrls: ['./inventory-list-detail.component.css']
+})
+export class InventoryListDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
