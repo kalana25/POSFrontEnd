8b1207ae61474ef1f1a1f7d9ad2a51e148d43365
RKalana <kalanamahaarachchi@gmail.com>
2020-09-07 23:28:50 +0530

    inventory pagination partial -1

--- src/app/sales/components/inventory/inventory-list/inventory-list.component.ts
+++ src/app/sales/components/inventory/inventory-list/inventory-list.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ResponseData } from 'src/app/core/response-data';
+import { InventoryPagination } from 'src/app/sales/models/inventory-pagination';
+import { RequestData } from 'src/app/core/request-data';
+import { InventoryService } from 'src/app/sales/services/inventory.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PageEvent } from '@angular/material';
 
 @Component({
   selector: 'app-inventory-list',
@@ -7,9 +13,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoryListComponent implements OnInit {
 
-  constructor() { }
+  grnResponse:ResponseData<InventoryPagination>;
+  grnRequest:RequestData
+  public IsLoading:boolean=false;
+
+  displayedColumns: string[] = ['id','action'];
+
+
+  constructor(
+    protected inventoryService:InventoryService,
+    protected route:ActivatedRoute,
+    protected router:Router) { 
+
+  }
 
   ngOnInit() {
+    this.grnRequest = new RequestData();
+    this.grnRequest.page=1;
+    this.grnRequest.pageSize=5;
+    this.getInventoryPagination(this.grnRequest);
+  }
+
+  private getInventoryPagination(grnRequest:RequestData) {
+    this.IsLoading = true;
+    this.inventoryService.pagination(grnRequest)
+    .subscribe(res=>{
+      this.grnResponse = res;
+      this.IsLoading = false;
+      console.log(res);
+    },err=>{
+      this.IsLoading = false;
+      console.error(err);
+    })
+  }
+
+  public OnPage(event:PageEvent):void {
+    this.grnRequest.pageSize = event.pageSize;
+    this.grnRequest.page= event.pageIndex+1;
+    this.getInventoryPagination(this.grnRequest);
   }
 
 }
