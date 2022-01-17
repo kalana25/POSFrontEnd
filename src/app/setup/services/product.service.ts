cc7bb041fdff2114a17b0621721f922ee8c7ceab
kalana <kal25mahaarachchi@gmail.com>
2022-01-17 23:50:12 +0530

    product add modified

--- src/app/setup/services/product.service.ts
+++ src/app/setup/services/product.service.ts
@@ -3,6 +3,8 @@ import { ApiService } from 'src/app/core/api-service';
 import { Product } from '../models/product';
 import { AppSettingsService } from 'src/app/core/app-settings.service'; 
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,13 @@ export class ProductService extends ApiService<Product>{
     protected config:AppSettingsService
   ) { 
     super(config.apiUrl,"Items",http);
+  }
+
+  getNextProductCode():Observable<any> {
+    return this.http.get<any>(`${this.config.apiUrl}/${this.resource}/find/next/code`)
+    .pipe(
+        tap(_=>console.log('fetched next product code')),
+        catchError(this.handleError<any>(`get=find/next/code`))
+    );
   }
 }
