d18c7a97b5e3ebd4057935ba0540083bc96df3cc
kalana <kal25mahaarachchi@gmail.com>
2022-01-17 23:04:51 +0530

    category add

--- src/app/setup/services/category.service.ts
+++ src/app/setup/services/category.service.ts
@@ -3,6 +3,8 @@ import { ApiService } from 'src/app/core/api-service';
 import { AppSettingsService } from 'src/app/core/app-settings.service'; 
 import { Category } from '../models/category';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +16,14 @@ export class CategoryService extends ApiService<Category>{
     protected config:AppSettingsService
   ) { 
     super(config.apiUrl,"ItemCategories",http);
+  }
+
+  getNextCategoryCode():Observable<any> {
+    return this.http.get<any>(`${this.config.apiUrl}/${this.resource}/find/next/code`)
+    .pipe(
+        tap(_=>console.log('fetched next category code')),
+        catchError(this.handleError<any>(`get=find/next/code`))
+    );
   }
 
   
