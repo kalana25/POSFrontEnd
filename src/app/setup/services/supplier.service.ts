f703ac209742e20dee6c60bef9e4eb31b803a560
kalana <kal25mahaarachchi@gmail.com>
2022-01-17 20:38:21 +0530

    category list

--- src/app/setup/services/supplier.service.ts
+++ src/app/setup/services/supplier.service.ts
@@ -3,6 +3,8 @@ import { ApiService } from 'src/app/core/api-service';
 import { AppSettingsService } from 'src/app/core/app-settings.service'; 
 import { Supplier } from '../models/supplier';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +16,13 @@ export class SupplierService extends ApiService<Supplier> {
     protected config:AppSettingsService
   ) {
     super(config.apiUrl,"Suppliers",http);
+  }
+
+  getNextSupplierCode():Observable<any> {
+    return this.http.get<any>(`${this.config.apiUrl}/${this.resource}/find/next/code`)
+    .pipe(
+        tap(_=>console.log('fetched next supplier code')),
+        catchError(this.handleError<any>(`get=find/next/code`))
+    );
   }
 }
